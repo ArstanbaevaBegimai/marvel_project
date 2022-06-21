d231b7f365fb4587d3eb0135387c786a348abbc7
Begimai <b.arstanbaeva@gmail.com>
2022-06-21 14:38:22 +0900

    marvel project

---
+++ src/components/errorMessage/ErrorMessage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ErrorMessage = () => {
+  return (
+    <img
+      src="https://monophy.com/media/22sG1VVP68WXv1JhiW/monophy.gif"
+      style={{
+        display: "block",
+        width: "250px",
+        height: "250px",
+        objectFit: "contain",
+        margin: "0 auto",
+      }}
+    />
+  );
+};
+
+export default ErrorMessage;
